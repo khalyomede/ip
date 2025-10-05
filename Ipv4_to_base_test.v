2f6abe81e82161073e6216c3a7ed34d9d85b5001
khalyomede <khalyomede@gmail.com>
2025-10-05 22:11:22 +0200

    add adjust Ipv4 to_base() test

--- Ipv4_to_base_test.v
+++ Ipv4_to_base_test.v
@@ -4,8 +4,8 @@ import ip { Ipv4 }
 import net { Ip }
 
 fn test_it_converts_ipv4_to_ip() {
-    address := Ipv4{address: [192, 168, 1, 1]}
-    ip := Ip{addr: [192, 168, 1, 1]}
+    address := Ipv4{address: [u8(192), 168, 1, 1]!}
+    ip := Ip{addr: [u8(192), 168, 1, 1]!}
 
     assert address.to_ip() == ip
 }
